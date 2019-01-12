7da22f8cb371db6eca3dca63fb96e52f4ed61f81
jgk83 <jgk83@cornell.edu>
2019-01-12 02:22:16 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    "httpPort": 3000,
+    "httpsPort": 3001,
+    "serverKey": path.join(__dirname,'server.key'),
+    "serverCert": path.join(__dirname,'server.crt'),
+    "dbLocation": "mongodb://localhost/activityPubBloggingPlatform",
+    "sessionSecret": "J4$58%*@AFJk%9(Ajfkajf83qf23fn8(bz)" //change me
+}
---
+++ index.js
@@ -0,0 +1,124 @@
+const config = require('./config.js');
+const spdy = require('spdy');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const favicon = require('serve-favicon');
+const MongoStore = require('connect-mongo')(session);
+const compression = require('compression');
+const db = require('monk')(config.dbLocation);
+
+
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const bcrypt = require('bcryptjs');
+// passport.use(new LocalStrategy({
+//     passReqToCallback: true
+// },function(req, username, password, done){
+//     // return done(null, {id:'wowow',name:'Josh'});
+//     Users.findOne({username: username}).then((doc) => {
+//         if(doc == null){
+//             // req.flash('message', {"error": 'Login Failed'});
+//             return done(null, false);
+//             // return done(null, false, {message: {"error": 'Login Failed'}});
+//         }
+//         var hash = doc.password;
+//         bcrypt.compare(password, hash).then((res) => {
+//             if(res == false){
+//                 // req.flash('message', {"error": 'Login Failed'});
+//                 return done(null, false);
+//                 // return done(null, false, {message: {"error": 'Login Failed'}});
+//             }else{
+//                 delete doc.password; // might not be necessary
+//                 return done(null, doc);
+//             }
+//         }).catch((err) => {
+//             return done(err);
+//         });
+//     }).catch((err) => {
+//         return done(err);
+//     });
+// }));
+//
+// passport.use('local-signup', new LocalStrategy({
+//     passReqToCallback: true
+// },
+//     function(req, username, password, done){
+//         if(config.restrictedNames.some((prefix) => {return username.toLowerCase().startsWith(prefix)})){
+//             // req.flash('message', {'error': 'Restricted username'});
+//             return done(null, false);
+//         }
+//         Users.findOne({username: username}).then((doc) => {
+//             if(doc != null){
+//                 // req.flash('message', {"error": 'User already exists'});
+//                 return done(null, false);
+//                 // return done(null, false, {message: {"error": 'User already exists'}});
+//             }else{
+//                 bcrypt.hash(password, 8, function(err, hash){
+//                     if(err) return done(err);
+//                     Users.insert({
+//                         username: username,
+//                         password: hash
+//                     }, function(err, user){
+//                         return done(err, user);
+//                     });
+//                 });
+//             }
+//         }).catch((err) => {
+//             return done(err);
+//     });
+// }));
+//
+// passport.serializeUser(function(user, done) {
+//     done(null, user._id);
+// });
+//
+// passport.deserializeUser(function(id, done) {
+//   Users.findOne({_id: id}, '-password', function(err, user){
+//       done(err, user);
+//   });
+// });
+
+const app = express();
+
+app.use(compression());
+
+// app.use(favicon(config.favicon));
+
+app.use(bodyParser.json({type: 'application/activity+json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: config.sessionSecret,
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+        url: config.dbLocation
+    })
+}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.end("meow");
+});
+
+//catch 404. this needs to be the last route
+app.use((req,res,next) => {
+    res.status(404).end('404');
+});
+
+const options = {
+    key: fs.readFileSync(config.serverKey),
+    cert:  fs.readFileSync(config.serverCert)
+};
+
+spdy.createServer(options, app).listen(config.httpsPort, function(err){
+    if(err){
+        console.error(err);
+        return process.exit(1);
+    }
+    console.log(`Listening on port: ${config.httpsPort}`);
+});
