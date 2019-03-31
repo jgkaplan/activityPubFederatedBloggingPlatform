15e5bb080289e66809a4dc7f5e5d0ad509bf4956
jgk83 <jgk83@cornell.edu>
2019-03-30 23:57:17 -0500

    Added caddy and vue

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     "serverKey": path.join(__dirname,'server.key'),
     "serverCert": path.join(__dirname,'server.crt'),
     "dbLocation": "mongodb://localhost/activityPubBloggingPlatform",
-    "sessionSecret": "J4$58%*@AFJk%9(Ajfkajf83qf23fn8(bz)" //change me
+    "sessionSecret": "J4$58%*@AFJk%9(Ajfkajf83qf23fn8(bz)", //change me
+    "privateKeyPassphrase": "ljkadf*33819(#*1b31b)" //change me
 }
--- index.js
+++ index.js
@@ -6,42 +6,43 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const favicon = require('serve-favicon');
-const MongoStore = require('connect-mongo')(session);
+// const MongoStore = require('connect-mongo')(session);
 const compression = require('compression');
-const db = require('monk')(config.dbLocation);
+// const db = require('monk')(config.dbLocation);
+const { generateKeyPair } = require('crypto');
+const cors = require('cors');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+passport.use(new LocalStrategy({
+    passReqToCallback: true
+},function(req, username, password, done){
+    // return done(null, {id:'wowow',name:'Josh'});
+    Users.findOne({username: username}).then((doc) => {
+        if(doc == null){
+            // req.flash('message', {"error": 'Login Failed'});
+            return done(null, false);
+            // return done(null, false, {message: {"error": 'Login Failed'}});
+        }
+        var hash = doc.password;
+        bcrypt.compare(password, hash).then((res) => {
+            if(res == false){
+                // req.flash('message', {"error": 'Login Failed'});
+                return done(null, false);
+                // return done(null, false, {message: {"error": 'Login Failed'}});
+            }else{
+                delete doc.password; // might not be necessary
+                return done(null, doc);
+            }
+        }).catch((err) => {
+            return done(err);
+        });
+    }).catch((err) => {
+        return done(err);
+    });
+}));
 
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const bcrypt = require('bcryptjs');
-// passport.use(new LocalStrategy({
-//     passReqToCallback: true
-// },function(req, username, password, done){
-//     // return done(null, {id:'wowow',name:'Josh'});
-//     Users.findOne({username: username}).then((doc) => {
-//         if(doc == null){
-//             // req.flash('message', {"error": 'Login Failed'});
-//             return done(null, false);
-//             // return done(null, false, {message: {"error": 'Login Failed'}});
-//         }
-//         var hash = doc.password;
-//         bcrypt.compare(password, hash).then((res) => {
-//             if(res == false){
-//                 // req.flash('message', {"error": 'Login Failed'});
-//                 return done(null, false);
-//                 // return done(null, false, {message: {"error": 'Login Failed'}});
-//             }else{
-//                 delete doc.password; // might not be necessary
-//                 return done(null, doc);
-//             }
-//         }).catch((err) => {
-//             return done(err);
-//         });
-//     }).catch((err) => {
-//         return done(err);
-//     });
-// }));
-//
 // passport.use('local-signup', new LocalStrategy({
 //     passReqToCallback: true
 // },
@@ -70,16 +71,16 @@ const db = require('monk')(config.dbLocation);
 //             return done(err);
 //     });
 // }));
-//
-// passport.serializeUser(function(user, done) {
-//     done(null, user._id);
-// });
-//
-// passport.deserializeUser(function(id, done) {
-//   Users.findOne({_id: id}, '-password', function(err, user){
-//       done(err, user);
-//   });
-// });
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Users.findOne({_id: id}, '-password', function(err, user){
+      done(err, user);
+  });
+});
 
 const app = express();
 
@@ -90,14 +91,14 @@ app.use(compression());
 app.use(bodyParser.json({type: 'application/activity+json'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: config.sessionSecret,
-    saveUninitialized: false,
-    resave: false,
-    store: new MongoStore({
-        url: config.dbLocation
-    })
-}));
+// app.use(session({
+//     secret: config.sessionSecret,
+//     saveUninitialized: false,
+//     resave: false,
+//     store: new MongoStore({
+//         url: config.dbLocation
+//     })
+// }));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
@@ -105,10 +106,59 @@ app.get('/', (req, res) => {
     res.end("meow");
 });
 
+app.options('/.well-known/webfinger', cors());
+
+app.get('/.well-known/webfinger', cors(), (req, res) => {
+    let queryString = req.query.resource;
+    
+});
+
+app.get('/signup', (req, res) => {});
+
+app.get('/login', (req, res) => {});
+
+app.post('/signup', (req, res) => {
+    // generateKeyPair('rsa',  {
+    //     modulusLength: 4096,
+    //     publicKeyEncoding: {
+    //         type: 'spki',
+    //         format: 'pem'
+    //     },
+    //     privateKeyEncoding: {
+    //         type: 'pkcs8',
+    //         format: 'pem',
+    //         cipher: 'aes-256-cbc',
+    //         passphrase: config.privateKeyPassphrase
+    //     }
+    // }, (err, publicKey, privateKey) => {
+    //     // Handle errors and use the generated key pair.
+    //     res.end(publicKey);
+    // });
+});
+
+app.post('/login', (req, res) => {});
+
+app.get('/settings', (req, res) => {});
+
+app.get('/dashboard', (req, res) => {});
+
+app.get('/followers', (req, res) => {});
+
+app.get('/following', (req, res) => {});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+
 //catch 404. this needs to be the last route
-app.use((req,res,next) => {
+app.all('*', (req,res) => {
     res.status(404).end('404');
 });
+// app.use((req,res,next) => {
+//     res.status(404).end('404');
+// });
 
 const options = {
     key: fs.readFileSync(config.serverKey),
